aa2009b4bc890f55a3368f2ebf3207dc843041ef
Casuy <casuistry016@gmail.com>
2018-09-21 23:29:40 +1000

    board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 class BoardSlot {
   letter: string;
@@ -14,16 +14,34 @@ class BoardSlot {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ui';
-  availableLetters = (new Array(199)).fill('a');
-  board = (new Array(20)).fill((new Array(20)).fill(new BoardSlot()));
+  // title = 'ui';
+  // availableLetters = (new Array(199)).fill('a');
+  board = (new Array(20))
+    .fill(1)
+    .map(() =>
+      (new Array(20))
+        .fill(1)
+        .map(() => new BoardSlot())
+    );
+  selectedSlot: BoardSlot;
+
+  @HostListener('document:keydown', ['$event.key']) onKeyDown(key) {
+    const isLetter = !!key.match(/^[a-z]$/i);
+    if (isLetter && !this.selectedSlot.letter) {
+      this.selectedSlot.letter = key;
+    }
+  }
 
   constructor() {
-    console.table(this.board);
+    // console.table(this.board);
   }
 
   pass() {
     const v = confirm('fjksdl');
     alert(v);
+  }
+
+  focus(x, y) {
+    this.selectedSlot = this.board[y][x];
   }
 }
